416108ff305438bae77b8472776845a4b39d6f0b
Deysi Morales Magadan <77804551+deysim@users.noreply.github.com>
2023-05-06 21:41:05 -0400

    first

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,82 @@
 // If you would like to see some examples of similar code to make an interface interact with an API, 
 // check out the coin-server example from a previous COMP 426 semester.
 // https://github.com/jdmar3/coinserver
+
+function showHideShots() {
+// Get the info from the checkbox
+  	let check = document.getElementById('opponent');
+  	let buttonrps = document.getElementById('rps');
+// Check if the checkbox is checked and show or hide options accordingly
+	if (check.checked == true) {
+		$('.shots').show()
+		if (buttonrps.checked == true) {
+			$('.rpsls').hide();
+		}
+
+	} else {
+		$('.shots').hide()
+	}
+}
+
+
+// This function clears the input form and also resets the shot selection
+// radio buttons. 
+function startOver () {
+	document.getElementById('userinput').reset();
+	showHideShots();
+}
+
+async function playGame () {
+	// Get which game is being played based on the value in the form
+	let game = $('input[type=radio][name=game]:checked').val();
+
+	// Get which shot is being played based on the value in the form
+	let shot = $('input[type=radio][name=shot]:checked').val();
+
+	//check if game is with opponent or not
+	let check = document.getElementById('opponent');
+
+	// Identify the base URL based on browser information
+	let baseurl = window.location.href + 'app/'
+
+	// Log the base URL
+	console.log(baseurl)
+
+	// This constructs a URL for the opponent option ONLY. To incorporate
+	// the other option, you can use a conditional to change the URL based
+	// on what is selected. You could also write separate functions, or use
+	// a conditional somewhere above in this function to construct the 
+	// correct URL
+	let urlone = baseurl + game + '/play/' + shot
+	let urltwo = baseurl + game + '/play/'
+	let url = baseurl;
+
+	if (check.checked == true) {
+		url = urlone;
+	} else {
+		url = urltwo;
+	}
+
+
+	let response = await fetch(url)
+	let result = await response.json()
+	// Log the result
+	console.log(result)
+	// Here you should include code that uses the DOM API or jQuery to 
+	// manipulate another block of HTML in the interface to display the 
+	// results in some way. 
+	let resulttext ='';
+
+	if (check.checked == true) {
+		resulttest = 'You: ' + result.player + ', Opponent: ' + result.opponent + ', Result: ' + result.result;
+	} else {
+		resulttest = 'You: ' + result.player
+	}
+
+	document.getElementById('results').innerText = resulttext;
+}
+
+function getRules(){
+	$('.playpage').hide();
+	$('.rules').show();
+}
--- server.js
+++ server.js
@@ -62,7 +62,57 @@ const app = express()
 // Set a port for the server to listen on
 const port = args.port || args.p || process.env.PORT || 8080
 // Load app middleware here to serve routes, accept data requests, etc.
-//
+import {rps, rpsls} from './lib/rpsls.js';
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.get('/app/', (req, res) => {
+    res.status(200).send("200 OK")
+})
+
+//Endpoint /app/rps/ that returns {"player":"(rock|paper|scissors)"}
+app.get('/app/rps/', (req, res) => {
+    res.status(200).send(JSON.stringify(rps()));
+})
+
+//Endpoint /app/rpsls/ that returns {"player":"(rock|paper|scissors|lizard|spock)"}
+app.get('/app/rpsls/', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls()));
+})
+
+//Endpoint /app/rps/play/ accepting request bodies
+app.get('/app/rps/play/', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.query.shot)));
+})
+
+//Endpoint /app/rpsls/play/ accepting request bodies
+app.get('/app/rpsls/play/', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
+})
+
+
+app.post('/app/rps/play/', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot)));
+})
+
+app.post('/app/rpsls/play/', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot)));
+})
+
+
+//endpoint with url
+app.get('/app/rps/play/:shot', (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.params.shot)));
+})
+
+app.get('/app/rpsls/play/:shot', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot)));
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('404 NOT FOUND');
+})
 // Create and update access log
 // The morgan format below is the Apache Foundation combined format but with ISO8601 dates
 app.use(morgan(':remote-addr - :remote-user [:date[iso]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent"',
